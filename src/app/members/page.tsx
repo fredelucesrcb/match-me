6ca7918c53a3bdbf8a8c07b815b94a1d47479396
Roy Fredeluces <fredeluces.rcb@gmail.com>
2025-03-29 01:22:26 +0800

    initial commit

---
+++ src/app/members/page.tsx
@@ -0,0 +1,15 @@
+import Link from 'next/link'
+import React from 'react'
+
+const MembersPage = () => {
+  return (
+    <div>
+        <h3>
+            This is the Members&#39; Page        
+        </h3>
+        <Link href={"/"}>Go back Home</Link>
+    </div>
+  )
+}
+
+export default MembersPage
